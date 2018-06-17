4e0b8a3ea96c90d38e97ca83b8c0dae549c499f9
LiBai666 <overwall2016@gmail.com>
2018-06-17 15:07:51 +0800

    使用writeStream写入文件，用mk_path进行路径操作

--- luxun.js
+++ luxun.js
@@ -6,22 +6,26 @@ const promisify = require("util").promisify;
 const p_access = promisify(fs.access);
 const p_mkdir = promisify(fs.mkdir);
 
+const mk_path_dir = require("mk_path666").dir;
+const p_mk_path_dir = promisify(mk_path_dir);
+
 function download(start,end){
     
     for (let i = start ; i <= end ; i++) {
       http.get(`http://www.kanunu8.com/book/4430/${i}.html `, function(res) {
             console.log(`getting ${i}.html`)
-            let dataBuf = null
+            //let dataBuf = null
+            const ws = fs.createWriteStream(path.join("./downloads/book/4430",`${i}.html`))
             res.on('data', function(chunk){
-              dataBuf += chunk
-              //以后再这里重构
+              //dataBuf += chunk
+              ws.write(chunk)
             });
             res.on('end', function(){
-                fs.writeFile(path.join("./downloads/book/4430/",`${i}.html`),dataBuf, (err) => {
+                /*fs.writeFile(path.join("./downloads/book/4430/",`${i}.html`),dataBuf, (err) => {
                     if (err) throw err;
                     console.log(`done ${i}.html`)
-                  });
-                console.log(`wirting ${i}.html`)
+                  });*/
+                console.log(`done ${i}.html`)
             });
       }).on('error', function(e) {
         console.log("Got error: " + e.message);
@@ -30,7 +34,7 @@ function download(start,end){
 }
 
 (async function(){
-  try{
+  /*try{
     await p_mkdir("./downloads")
   }catch(e){}
 
@@ -40,10 +44,14 @@ function download(start,end){
 
   try{
     await p_mkdir("./downloads/book/4430")
-  }catch(e){}
-
+  }catch(e){}*/
+  try{
+    await p_mk_path_dir("./downloads/book/4430")
+  }catch(e){
+    throw e
+  }
   download(55262,55295);
   
 })();
 
-setTimeout(()=>{},1111111111);
+//setTimeout(()=>{},1111111111);
